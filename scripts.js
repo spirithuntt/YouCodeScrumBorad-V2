32a3e9ebf8165d27d9628a4140ecf5b1de029741
spirithuntt <nouhaila.elhousnii@gmail.com>
2022-10-30 13:07:16 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,49 +1,47 @@
 /*********************************************/
-let id = document.getElementById("task-id").value
+let id = document.getElementById("task-id").value;
 let title = document.getElementById("task-title");
-let bug = document.getElementById("task-type-feature");
-let feature = document.getElementById("task-type-bug");
+let bug = document.getElementById("task-type-bug");
+let feature = document.getElementById("task-type-feature");
 let priority = document.getElementById("task-priority");
-let status= document.getElementById("task-status");
-let date= document.getElementById("task-date");
-let description= document.getElementById("task-description");
-
-
-
-
-
-
-
-
+let status = document.getElementById("task-status");
+let date = document.getElementById("task-date");
+let description = document.getElementById("task-description");
 
 document.getElementById("addBtn").addEventListener("click", btnswitch);
-function btnswitch()//SWITCH BUTTONS
- {
-    document.getElementById("task-save-btn").style.display='block';
-    document.getElementById("task-update-btn").style.display='none';
-    document.getElementById("task-delete-btn").style.display='none';
-    document.getElementById("task-save-btn").setAttribute("type", "submit");
-    document.getElementById("task-save-btn").setAttribute("name", "save");
+function btnswitch() {
+  //SWITCH BUTTONS
+  document.getElementById("task-save-btn").style.display = "block";
+  document.getElementById("task-update-btn").style.display = "none";
+  document.getElementById("task-delete-btn").style.display = "none";
+  document.getElementById("task-save-btn").setAttribute("type", "submit");
+  document.getElementById("task-save-btn").setAttribute("name", "save");
 }
-function editTask(id)
-{
-  document.getElementById("task-delete-btn").style.display='block';
-  document.getElementById("task-update-btn").style.display='block';
-  document.getElementById("task-save-btn").style.display='none';
-  document.getElementById("task-update-btn").addEventListener("click", setAttributeupdate);
-  document.getElementById("task-delete-btn").addEventListener("click", setAttributedelete);
+//display data in modal
+function editTask(id) {
+  document.getElementById("task-delete-btn").style.display = "block";
+  document.getElementById("task-update-btn").style.display = "block";
+  document.getElementById("task-save-btn").style.display = "none";
+  document
+    .getElementById("task-update-btn")
+    .addEventListener("click", setAttributeupdate);
+  document
+    .getElementById("task-delete-btn")
+    .addEventListener("click", setAttributedelete);
   //input
   let test = document.getElementById(id);
   title.value = test.children[1].children[0].innerHTML;
-   if(test.children[1].children[2].children[2].innerHTMLt=="1") {feature.checked=true} else {bug.checked=true}
+
+  if (test.children[1].children[2].children[2].innerHTML == "1") {
+    feature.checked = true;
+  } else {
+    bug.checked = true;
+  }
+
   description.value = test.children[1].children[1].children[1].innerHTML;
-
   priority.value = test.children[1].children[2].children[1].innerHTML;
-
-  console.log();
- 
   date.value = test.children[1].children[1].children[0].innerHTML.slice(-10);
-
+  console.log(test.children[1].children[2].children[2].innerHTML);
   if (test.parentElement.id == "to-do-tasks") {
     status.value = 1;
   } else if (test.parentElement.id == "in-progress-tasks") {
@@ -51,16 +49,12 @@ function editTask(id)
   } else if (test.parentElement.id == "done-tasks") {
     status.value = 3;
   }
-
-
-
 }
-function setAttributedelete(){
+function setAttributedelete() {
   document.getElementById("task-delete-btn").setAttribute("type", "submit");
   document.getElementById("task-delete-btn").setAttribute("name", "save");
 }
-function setAttributeupdate(){
+function setAttributeupdate() {
   document.getElementById("task-update-btn").setAttribute("type", "submit");
   document.getElementById("task-update-btn").setAttribute("name", "save");
 }
-
